6fd4d15d9f1ec91d12c31b1b03c2dbe4e4983390
shusainov <shusainov3@gmail.com>
2022-03-03 11:06:48 +0500

    Change some xpaths

--- src/main/java/CyberArkPassword.java
+++ src/main/java/CyberArkPassword.java
@@ -26,9 +26,7 @@ public class CyberArkPassword {
 
         driver.get(conf.getProperty("URL"));
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("authItem")));
-        List<WebElement> elements = driver.findElements(By.id("authItem"));
-        elements.get(1).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'LOGON')]"))).click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("i0116")));
         driver.findElement(By.id("i0116")).sendKeys(conf.getProperty("email"));
@@ -48,16 +46,13 @@ public class CyberArkPassword {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("frame-epv")));
         driver.switchTo().frame("frame-epv");
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"clipboardenabled\"]/div/pv-accounts-actions-manager/pv-accounts/pv-accounts-splitter/cyb-splitter/div/div[1]/pv-accounts-grid/cyb-server-data-table/div[1]/ag-grid-angular/div/div[2]/div[1]/div[3]/div[3]/div/div/div/div/cyb-actions-col/div/cyb-more-items-trigger-action-menu/cyb-floating-container/div/span/span/cyb-more-items-trigger/button")));
-        driver.findElement(By.xpath("//*[@id=\"clipboardenabled\"]/div/pv-accounts-actions-manager/pv-accounts/pv-accounts-splitter/cyb-splitter/div/div[1]/pv-accounts-grid/cyb-server-data-table/div[1]/ag-grid-angular/div/div[2]/div[1]/div[3]/div[3]/div/div/div/div/cyb-actions-col/div/cyb-more-items-trigger-action-menu/cyb-floating-container/div/span/span/cyb-more-items-trigger/button")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(@class,'more-item')]"))).click();
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"clipboardenabled\"]/div/router-outlet/div/div/span/cyb-action-menu/div/div/div/cyb-action-menu-item[1]/div/span")));
-        driver.findElement(By.xpath("//*[@id=\"clipboardenabled\"]/div/router-outlet/div/div/span/cyb-action-menu/div/div/div/cyb-action-menu-item[1]/div/span")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='dialog']//input[contains(@formcontrolname,'secret')]")));
 
-        System.out.println(driver.findElement(By.xpath("//*[@id=\"clipboardenabled\"]/div/pv-accounts-actions-manager/pv-accounts-show-dialog/cyb-modal/div/div/section/div[2]/div/pv-accounts-show-dialog-content/div/cyb-input-field/div/div[2]/div/div/input")).getAttribute("value"));
+        System.out.println(driver.findElement(By.xpath("//div[@role='dialog']//input[contains(@formcontrolname,'secret')]")).getAttribute("value"));
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"clipboardenabled\"]/div/pv-accounts-actions-manager/pv-accounts-show-dialog/cyb-modal/div/div/section/div[2]/div/pv-accounts-show-dialog-content/div/div/button/span")));
-        driver.findElement(By.xpath("//*[@id=\"clipboardenabled\"]/div/pv-accounts-actions-manager/pv-accounts-show-dialog/cyb-modal/div/div/section/div[2]/div/pv-accounts-show-dialog-content/div/div/button/span")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='dialog']//button"))).click();
     }
 }
 
